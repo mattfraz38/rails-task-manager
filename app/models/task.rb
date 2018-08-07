1346baa60146a86ecf259e683960cede437a9169
Matthew Frazier <frazier3388@yahoo.com>
2018-08-07 21:01:46 +0900

    Completed Task Manager No CSS

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  # validations go here
+
+  # SPECIES = %w(cat dog horse bird turtle)
+  # Ex.  validates :name, presence: true
+  #      validates :found_on
+  # =>   validates :species, inclusion: { in: SPECIES }
+  # Test if these work in the console with rails c
+  #                                        seabiscuit..valid?
+  #                                        seabiscuit.name = "Seabiscuit"
+  #                                        Seabiscuit.save
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
+  # long way:
+  # get "/pets", to: "pets#index"
+  # get "/pets/new", to: "pets#new", as: :new_pet
+  # get "/pets/:id", to: "pets#show"
+  # post "/pets/", to: "pets#create"
 end
